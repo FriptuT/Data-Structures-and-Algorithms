b07680ad9a65e205555bc92c19e776265fb9f6a4
FriptuT <friptut@gmail.com>
2024-03-07 15:22:38 +0200

    ++usoare

--- pbinfo/usoare/p5/produsMaxime.cpp
+++ pbinfo/usoare/p5/produsMaxime.cpp
@@ -1,39 +1,81 @@
+/*
+    Se citesc de la tastatură numere naturale până la apariția lui zero.
+    Să se determine cea mai mare valoare care poate obține înmulțind doua valori diferite dintre cele citite.
+*/
+
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-int n,x;
+void produsMaximTreiValori(){
+    int max1,max2,max3, x,Pr;
+    max1 = 0;
+    max2 = 0;
+    max3 = 0;
+
+    while (x != 0)
+    {
+        cout << "x=";
+        cin >> x;
+
+
+       if (x > max1)
+       {
+            max3 = max2;
+            max2 = max1;
+            max1 = x;
+       }
+       else if(x > max2 && x != max1 && x != max3)
+       {
+            max2 = x;
+       }
+       else if(x > max3 && x != max1 && x != max2)
+       {
+            max3 = x;
+       }
+
+       
+    }
+
+    Pr = 1;
+    Pr *= (max1 * max2 * max3);
+    cout << Pr << " este valoarea cea mai mare care se poate obtine";
+}
+
+void produsMaximDouaValori(){
+    int max1,max2, x,Pr;
+    max1 = 0;
+    max2 = 0;
+
+    while (x != 0)
+    {
+        cout << "x=";
+        cin >> x;
+
+
+       if (x > max1)
+       {
+            max2 = max1;
+            max1 = x;
+       }
+       else if(x > max2 && x != max1)
+       {
+            max2 = x;
+       }
+
+    }
+
+    Pr = 1;
+    Pr *= (max1 * max2);
+    cout << Pr << " este valoarea cea mai mare care se poate obtine";
+}
 
 int main()
 {
-    int max1 = 0, max2 = 0;
-    while (true)
-    {
-        cin>>x;
-        if (x == 0)
-        {
-            break;
-        }
+    produsMaximDouaValori();
 
-        if (x > max1)
-        {
-            max2 = max1;
-            max1 = x;
-        }
-        else if (x > max2 && x != max1)
-        {
-            max2 = x;
-        }
-        
-        
-        
-    }
+    cout<<endl;
     
-    cout<<max1*max2;
-    
-
-
-
+    produsMaximTreiValori();
 
     return 0;
 }
