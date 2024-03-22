36b38605ffdf6d8603e3d17993d5327e2ada7acb
FriptuT <friptut@gmail.com>
2024-03-22 07:54:47 +0200

    ++digits_chapter

--- pbinfo/medie/p7/ADIDAS.cpp
+++ pbinfo/medie/p7/ADIDAS.cpp
@@ -1,35 +1,75 @@
 // Un număr natural se numește număr ADIDAS dacă:
 
-//are exact șase cifre;
-//prima cifră, a doua, a treia și ultima sunt diferite două câte două;
-//penultima cifră este egală cu prima;
-//a patra cifră este egală cu a doua.
-//De exemplu, numărul 459540 este număr ADIDAS, iar 459549 nu este număr ADIDAS.
+// are exact șase cifre;
+// prima cifră, a doua, a treia și ultima sunt diferite două câte două;
+// penultima cifră este egală cu prima;
+// a patra cifră este egală cu a doua.
+// De exemplu, numărul 459540 este număr ADIDAS, iar 459549 nu este număr ADIDAS.
 
-//Se dă un șir cu n numere naturale. 
-//Să se determine cel mai mare număr ADIDAS din șir.
+// Se dă un șir cu n numere naturale.
+// Să se determine cel mai mare număr ADIDAS din șir.
 #include <iostream>
 using namespace std;
 
-int n,x,el,cif;
+int n, x, el, cif,ok;
+
+bool isADIDAS(int numar){
+    if (numar < 100000 || numar > 999999)
+    {
+        return false; // numarul nu are 6 cifre
+    }
+
+    int sixth = numar % 10;
+    numar /= 10;
+    int fifth = numar % 10;
+    numar /= 10;
+    int fourht = numar % 10;
+    numar /= 10;
+    int third = numar % 10;
+    numar /= 10;
+    int second = numar % 10;
+    numar /= 10;
+    int first = numar;
+    
+    if (first == second && first == third && first == sixth
+    && second == third && second == sixth
+    && third == sixth)
+    {
+        return false;
+    }
+
+    if (fifth != first)
+    {
+        return false;
+    }
+
+    if (fourht != second)
+    {
+        return false;
+    }
+}
+
+// int n, x, el, cif,ok;
 
 int main()
 {
     cout<<"n=";cin>>n;
+    int maxAdidas = -1;
     for (int i = 1; i <= n; i++)
     {
-        cout<<"numar=";cin>>x;
+        cout<<"numar = ";cin>>x;
 
-        while (el != 0 && el <= 999999)
+        if (isADIDAS(x))
         {
-            int prima = el / 100000;
-            
-            
+            if (x > maxAdidas)
+            {
+                maxAdidas = x;
+            }
         }
-        
     }
+
+    cout<<"Numarul ADIDAS maxim este : "<<maxAdidas;
     
 
-
     return 0;
-}   
+}
--- pbinfo/usoare/2.11to20/p17/adaugaCif.cpp
+++ pbinfo/usoare/2.11to20/p17/adaugaCif.cpp
@@ -12,12 +12,8 @@ int main()
 
     el = n;                             // copie numar
     sumaCif = 0;                        // sumaCifre
-    // ultimaCifra = el % 10;              // ultimaCifra
-    // sumaCif = sumaCif  + ultimaCifra;   // adunam ultimaCifra la suma
     while (el != 0)
     {
-        // n = 3712531
-        // cifCurenta = (el % 10 == ultimaCifra) ? ((el / 10) %10) : (el % 10);
         cifCurenta = el % 10;
         
         if (cifCurenta > cifMaxima)
