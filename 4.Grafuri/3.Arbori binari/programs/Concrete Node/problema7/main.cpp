7913ce5efbfc05883337aef643965312c0054b56
FriptuT <friptut@gmail.com>
2024-03-26 16:48:45 +0200

    end section displaying-concrete-node

---
+++ 4.Grafuri/3.Arbori binari/programs/Concrete Node/problema7/main.cpp
@@ -0,0 +1,48 @@
+/*
+    Nodurile a caror informatie este egala cu valoarea "val"
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int n, rad, nod, st[100], dr[100];
+int info[100];
+
+void informatieNoduri(int rad)
+{
+
+    if (info[rad] == 50)
+    {
+        cout << rad << " ";
+    }
+    else
+    {
+        if (st[rad] != 0)
+        {
+            informatieNoduri(st[rad]);
+        }
+
+        if (dr[rad] != 0)
+        {
+            informatieNoduri(dr[rad]);
+        }
+    }
+}
+
+int main()
+{
+    ifstream inFile("input.txt");
+
+    inFile >> n >> rad;
+    for (int i = 1; i <= n; i++)
+    {
+        inFile>>st[i]>>dr[i]>>info[i];
+    }
+    inFile.close();
+
+    informatieNoduri(rad);
+    
+
+    return 0;
+}
